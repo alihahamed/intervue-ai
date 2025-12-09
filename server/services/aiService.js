131ec05428dd52a960a3a7394b76862639aa8991
Ali Ahmed <aliahmedyus@gmail.com>
2025-12-09 22:49:24 +0530

    brr

--- server/services/aiService.js
+++ server/services/aiService.js
@@ -1,31 +1,22 @@
 import fetch from 'node-fetch'
+import Groq from 'groq-sdk/index.mjs';
 
 export async function getAiResponse(userTranscript, sysInstructions) {
-  const systemPrompt = `You are a strict Senior Software Engineer conducting a job interview. 
-        The candidate just gave an answer (provided in text). 
-        
-        1. Grade their answer from 1-10.
-        2. Briefly explain why.
-        
-        
-        Keep your response short but professional.`;
+  
+  const groq = new Groq({apiKey:"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"})
 
   try {
-    const response = await fetch("http://127.0.0.1:11434/api/chat", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        model: "llama3.2",
-        messages: [
-          { role: "system", content: sysInstructions },
-          { role: "user", content: userTranscript },
-        ],
-        stream:false
-      }),
+    const completion = await groq.chat.completions.create({
+      messages: [
+        { role: "system", content: sysInstructions},
+        { role: "user", content: userTranscript },
+      ],
+      model: "llama-3.3-70b-versatile", 
+      temperature: 0.5, 
     });
 
-    const data = await response.json();
-    return data.message.content;
+   return completion.choices[0]?.message?.content || "No response generated.";
+   
   } catch (error) {
     console.log("Error while fetching the ai response", error)
   }
--- server/services/ttsService.js
+++ server/services/ttsService.js
@@ -14,7 +14,7 @@ export async function TextToSpeech(text, filePath) {
       OUTPUT_FORMAT.WEBM_24KHZ_16BIT_MONO_OPUS
     );
     
-    const {audioStream} = await tts.toStream(text)
+    const {audioStream} = await tts.toStream(text, {rate: 1, pitch: "-50Hz"})
 
     const audioBuffer = await new Promise((resolve, reject) => {
       const chunks = [];
