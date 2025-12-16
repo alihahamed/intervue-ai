cf8a136ade11184869a4696a0bb5b36402896971
Ali Ahmed <aliahmedyus@gmail.com>
2025-12-16 21:30:32 +0530

    brr

--- client/src/components/chat.jsx
+++ client/src/components/chat.jsx
@@ -79,7 +79,7 @@ function ChatConversation() {
           SLM's
         </HoverBorderGradient>
       </div>
-      <Card className="relative mx-auto items-center justify-center w-[90%] md:w-full max-w-2xl h-[620px] md:h-[450px] xl:h-[500px] 2xl:h-[520px] mt-14 bg-[#09090b]/90 border border-[#27272a] shadow-2xl rounded-xl overflow-hidden backdrop-blur-sm flex flex-col transition-all duration-300">
+      <Card className="relative mx-auto items-center justify-center w-[90%] md:w-full max-w-3xl h-[620px] md:h-[450px] xl:h-[500px] 2xl:h-[480px] mt-10 bg-[#09090b]/90 border border-[#27272a] shadow-2xl rounded-xl overflow-hidden backdrop-blur-sm flex flex-col transition-all duration-300">
         <div className="flex h-full flex-col z-10 relative w-full">
           <Conversation className="flex-1 overflow-y-auto overflow-x-hidden">
             <ConversationContent className="p-2 md:p-4 space-y-4">
